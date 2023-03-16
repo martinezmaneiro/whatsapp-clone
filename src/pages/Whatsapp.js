319d9983dc587862cbf29d9bf78225e3c4c97a8e
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-16 06:32:05 -0300

    [ADDED] containers style

--- src/pages/Whatsapp.js
+++ src/pages/Whatsapp.js
@@ -4,9 +4,14 @@ import MainScreen from '../components/MainScreen';
 const Whatsapp =()=> {
     return (
         <div className='w-screen h-screen overflow-hidden'>
-            <div className='flex justify-start whatsapp-bp:justify-center item-center bg-[#111a21] h-screen'></div>
-            <Sidebar/>
-            <MainScreen/>
+            <div className='flex justify-start whatsapp-bp:justify-center item-center bg-[#111a21] h-screen'>
+                <div className='bg-[#111a21] min-w-[340px] max-w-[500px] w-100 h-100'>
+                    <Sidebar/>
+                </div>
+                <div className='bg-[#222f35] min-w-[415] max-w-[1120px] w-100 h-100'>
+                    <MainScreen/>
+                </div>
+            </div>
         </div>
     )
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js, jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screen: {
+        'whatsapp-bp': '755px'
+      }
+    },
   },
   plugins: [],
 }
