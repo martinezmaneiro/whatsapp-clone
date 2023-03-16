2c01ed3191cc07270585e429f8a6f28bd66f51dc
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-16 06:54:54 -0300

    [ADDED] button component

--- src/components/ProfileNav.js
+++ src/components/ProfileNav.js
@@ -1,6 +1,8 @@
 const ProfileNav =()=> {
     return (
-        <div></div>
+        <div className='flex justify-between items-center bg-[#202d33] h-[60px] p-3'>
+            <img src='' alt='profile picture'/>
+        </div>
     )
 }
 
