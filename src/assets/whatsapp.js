3795e76a02462f2bd3a9384360325fc1090ecfed
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-16 08:17:00 -0300

    [ADDED] assets

---
+++ src/assets/whatsapp.js
@@ -0,0 +1,14 @@
+import chat1 from './images/chat1';
+import chat2 from './images/chat2';
+import chat3 from './images/chat3';
+import chat4 from './images/chat4';
+import chat5 from './images/chat5';
+import chat6 from './images/chat6';
+import chat7 from './images/chat7';
+import chat8 from './images/chat8';
+import chat9 from './images/chat9';
+import chat10 from './images/chat10';
+
+const photoUrl = 'https://avatars.githubusercontent.com/u/112190072?s=400&u=96c03d2229f6cd2b36cf0f94ca08ec161b259dd1&v=4';
+
+export { chat1, chat2, chat3, chat4, chat5, chat6, chat7, chat8, chat9, chat10, photoUrl};
