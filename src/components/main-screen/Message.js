1c935cca6acf364eb3d56c2d9e96032d9701cd4f
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-20 10:39:56 -0300

    [ADDED] link message and img message display

--- src/components/main-screen/Message.js
+++ src/components/main-screen/Message.js
@@ -5,12 +5,32 @@ const Message =({ msg, time, img, isLink, sent})=> {
         }`}>
             {img ?
                 (<div className="relative w-100 p-2">
-                    <img src={img} alt="img message" className="rounded-md max-w-[270px] w-100"/>
+                    <img
+                        src={img}
+                        alt="img message"
+                        className="rounded-md max-w-[270px] w-100"
+                    />
                     <p className="absolute right-2 bottom-3 text-[#8796a1] text-[10px] min-w-[50px]">{time}</p>
                 </div>)
                 :
-                <h1></h1>
-            };
+                (<div
+                    className="flex justify-between items-end max-w-[410px] p-2"
+                    style={{ wordBreak: 'break-word'}}
+                >
+                    {isLink ?
+                    (<a
+                        href={msg}
+                        target='_blank'
+                        className="text-[#53beec] hover:text-[#53beec] focus:text-[#53beec] active:text-[#53beec] text-sm underline hover:underline mr-2"
+                        >
+                        {msg}
+                    </a>
+                    ):(
+                        <p className="text-white text-sm mr-2">{msg}</p>
+                    )}
+                    <p>{time}</p>
+                </div>)
+            }
         </div>
     )
 };
