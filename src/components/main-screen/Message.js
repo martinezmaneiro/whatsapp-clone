8ac2af671960c10403ab0233168261e705b2d475
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-20 10:09:38 -0300

    [FIXED] Message component img prop call

--- src/components/main-screen/Message.js
+++ src/components/main-screen/Message.js
@@ -2,7 +2,16 @@ const Message =({ msg, time, img, isLink, sent})=> {
     return (
         <div className={`flex justify-center items-center rounded-md w-fit my-1 ${
             sent ? 'bg-[#005C4B]' : 'bg-[#202d33]'
-        }`}>Message</div>
+        }`}>
+            {img ?
+                (<div className="relative w-100 p-2">
+                    <img src={img} alt="img message" className="rounded-md max-w-[270px] w-100"/>
+                    <p className="absolute right-2 bottom-3 text-[#8796a1] text-[10px] min-w-[50px]">{time}</p>
+                </div>)
+                :
+                <h1></h1>
+            };
+        </div>
     )
 };
 
