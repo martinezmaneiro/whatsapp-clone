766ccee8759c37636a9ffd0eb0ee79a51ae374c3
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-20 10:48:05 -0300

    [ADDED] message alignment by author

--- src/components/main-screen/Message.js
+++ src/components/main-screen/Message.js
@@ -1,7 +1,7 @@
 const Message =({ msg, time, img, isLink, sent})=> {
     return (
         <div className={`flex justify-center items-center rounded-md w-fit my-1 ${
-            sent ? 'bg-[#005C4B]' : 'bg-[#202d33]'
+            sent ? 'bg-[#005C4B] ml-auto' : 'bg-[#202d33] mr-auto'
         }`}>
             {img ?
                 (<div className="relative w-100 p-2">
