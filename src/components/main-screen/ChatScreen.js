6d547db411a69824b184dd8ccc58899a8348b02c
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-20 09:20:33 -0300

    [ADDED] main-bacground

--- src/components/main-screen/ChatScreen.js
+++ src/components/main-screen/ChatScreen.js
@@ -1,9 +1,13 @@
 import { ContactBar } from './ContactBar';
+import { InputBar } from './InputBar';
+import { Conversation } from './Conversation';
 
 const ChatScreen =()=> {
     return (
         <div className='flex flex-col h-screen'>
             <ContactBar/>
+            <Conversation/>
+            <InputBar/>
         </div>
     )
 };
---
+++ src/components/main-screen/Conversation.js
@@ -0,0 +1,5 @@
+export const Conversation =()=> {
+    return (
+        <div className="bg-[#0a131a] bg-[url('assets/images/bg.webp')] bg-contain overflow-y-scroll h-100 py-3"></div>
+    )
+};
