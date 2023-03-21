358c8452207bd4f2cf1ba444cc9f098b44bcd863
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-21 10:46:48 -0300

    [FIXED] loading page display

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -7,14 +7,14 @@ export const Loading = ({progress})=> {
         <div>
             <div className="flex flex-col justify-center items-center bg-[#111a21] w-screen h-screen">
                 <span className="text-[#3d464a] text-6xl my-12"><BsWhatsapp/></span>
-            </div>
-            <div className="flex flex-col justify-evenly items-center h-[150px]">
-                <ProgressBar variant='success' now={progress} className='bg-[#243138] rounded-lg w-[320px] h-[3px]'/>
-                <div className="flex flex-col items-center">
-                    <h1 className="text-[#c1c6c9] text-lg font-medium">WhatsApp</h1>
-                    <div className='flex items-center text-[#687782]'>
-                        <span className="text-sm mr-3"><FaLock/></span>
-                        <p>End-to-end encrypted</p>
+                <div className="flex flex-col justify-evenly items-center h-[150px]">
+                    <ProgressBar variant='success' now={progress} className='bg-[#243138] rounded-lg w-[320px] h-[3px]'/>
+                    <div className="flex flex-col items-center">
+                        <h1 className="text-[#c1c6c9] text-lg font-medium">WhatsApp</h1>
+                        <div className='flex items-center text-[#687782]'>
+                            <span className="text-sm mr-3"><FaLock/></span>
+                            <p>End-to-end encrypted</p>
+                        </div>
                     </div>
                 </div>
             </div>
